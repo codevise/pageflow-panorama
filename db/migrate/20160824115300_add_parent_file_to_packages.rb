a6675991eafbf26ce136923af7a73b732a47a54e
Tim Fischbach <tfischbach@codevise.de>
2017-08-03 15:37:12 +0200

    Migration added index for video files instead of packages

--- db/migrate/20160824115300_add_parent_file_to_packages.rb
+++ db/migrate/20160824115300_add_parent_file_to_packages.rb
@@ -2,7 +2,7 @@ class AddParentFileToPackages < ActiveRecord::Migration
   def change
     add_column :pageflow_panorama_packages, :parent_file_id, :integer
     add_column :pageflow_panorama_packages, :parent_file_model_type, :string
-    add_index :pageflow_video_files, [:parent_file_id, :parent_file_model_type],
+    add_index :pageflow_panorama_packages, [:parent_file_id, :parent_file_model_type],
               name: 'index_package_files_on_parent_id_and_parent_model_type'
   end
 end
